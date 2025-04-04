3e217405b74d119cbe799541816742ecc2357488
Lilofer <emifernandez@alumnos.uai.cl>
2025-04-04 19:25:37 -0300

    Update metrics.c

--- archivos_c/metrics.c
+++ archivos_c/metrics.c
@@ -6,6 +6,8 @@
 #define MAX_LINE 1024
 #define MAX_ORDERS 1000
 #define MAX_FIELD_LENGTH 200
+#define MAX_CATEGORIES 20 //Para funcion hp
+#define MAX_INGREDIENTS 100 // Maxima cantidad de ingredientes unicos, Para funcion ims
 
 typedef struct {
     char pizza_id[20];
@@ -94,6 +96,12 @@ void load_csv(const char *filename) {
 }
 
 // === FUNCIONES DE MÉTRICAS ===
+void trim_newline(char *str) {
+    size_t len = strlen(str);
+    if (len > 0 && str[len - 1] == '\n') {
+        str[len - 1] = '\0';
+    }
+}
 
 void pms() {
     char best_seller[50];
@@ -270,6 +278,87 @@ void apd() {
     float promedio = (float)total_pizzas / fechas_count;
     printf("Promedio de pizzas por día: %.2f\n", promedio);
 }
+void hp() {
+    int category_counts[MAX_CATEGORIES] = {0};
+    char category_names[MAX_CATEGORIES][20];
+    int category_count = 0;
+
+    for (int i = 0; i < order_count; i++) {
+        if (strlen(orders[i].pizza_category) == 0) continue;
+
+        char cleaned_category[20];
+        strncpy(cleaned_category, orders[i].pizza_category, sizeof(cleaned_category) - 1);
+        cleaned_category[sizeof(cleaned_category) - 1] = '\0';
+        trim_newline(cleaned_category);
+
+        int found = 0;
+        for (int j = 0; j < category_count; j++) {
+            if (strcmp(cleaned_category, category_names[j]) == 0) {
+                category_counts[j] += orders[i].quantity;
+                found = 1;
+                break;
+            }
+        }
+        if (!found && category_count < MAX_CATEGORIES) {
+            strncpy(category_names[category_count], cleaned_category, sizeof(category_names[category_count]) - 1);
+            category_names[category_count][sizeof(category_names[category_count]) - 1] = '\0';
+            category_counts[category_count] = orders[i].quantity;
+            category_count++;
+        }
+    }
+    for (int i = 0; i < category_count; i++) {
+        printf("Categoría: %s, Cantidad: %d\n", category_names[i], category_counts[i]);
+    }
+}
+
+void ims() {
+    int ingredient_counts[MAX_INGREDIENTS] = {0};
+    char ingredient_names[MAX_INGREDIENTS][50] = {0};
+    int ingredient_count = 0;
+
+    for (int i = 0; i < order_count; i++) {
+        char ingredients[MAX_FIELD_LENGTH];
+        strcpy(ingredients, orders[i].pizza_ingredients);
+        char *ingredient = strtok(ingredients, ",");
+        while (ingredient != NULL) {
+            // Trim leading/trailing spaces
+            while (*ingredient == ' ') ingredient++;
+            char *end = ingredient + strlen(ingredient) - 1;
+            while (end > ingredient && *end == ' ') end--;
+            *(end + 1) = '\0';
+
+            int found = 0;
+            for (int j = 0; j < ingredient_count; j++) {
+                if (strcmp(ingredient, ingredient_names[j]) == 0) {
+                    ingredient_counts[j] += orders[i].quantity;
+                    found = 1;
+                    break;
+                }
+            }
+            if (!found && ingredient_count < MAX_INGREDIENTS) {
+                strcpy(ingredient_names[ingredient_count], ingredient);
+                ingredient_counts[ingredient_count] = orders[i].quantity;
+                ingredient_count++;
+            }
+            ingredient = strtok(NULL, ",");
+        }
+    }
+
+    int max_count = 0;
+    int max_index = -1;
+    for (int i = 0; i < ingredient_count; i++) {
+        if (ingredient_counts[i] > max_count) {
+            max_count = ingredient_counts[i];
+            max_index = i;
+        }
+    }
+
+    if (max_index != -1) {
+        printf("El ingrediente más vendido es: %s\nCon un total de %d ventas \n", ingredient_names[max_index], ingredient_counts[max_index]);
+    } else {
+        printf("No se encontraron ingredientes.\n");
+    }
+}
 
 
 // === MAPEO DE FUNCIONES ===
@@ -290,6 +379,8 @@ FunctionMap functions[] = {
     {"dlsp", dlsp},
     {"apo", apo},
     {"apd", apd},
+    {"hp", hp},
+    {"ims", ims}
 };
 
 int num_functions = sizeof(functions) / sizeof(FunctionMap);
